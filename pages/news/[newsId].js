57b38501bc46eb4fd02b480d6063eb67e22880b5
Debadrita Bandyopadhyay <bristibandyopadhyay@gmail.com>
2021-10-23 14:35:47 +0530

    Adding link to navigate to different page

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,6 +1,13 @@
 // our-domain.com/news/(something-important) <--- dynamic Parameter
 
+import { useRouter } from 'next/router';
+
 const DetailsPage = () => {
+    const router = useRouter();
+
+    const newsId = router.query.newsId;
+    // send request to the backend to fetch data
+
     return <h1>The Details Page</h1>;
 };
 
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,22 @@
 // our-domain.com/news
 
+import { Fragment } from 'react';
+import Link from 'next/link';
+
 const NewsPage = () => {
-    return <h1>The News Page</h1>;
+    return (
+        <Fragment>
+            <h1>The News Page</h1>
+            <ul>
+                <li>
+                    <Link href='/news/next-js-is-a-great-framework'>NextJS is a Great Framework</Link>
+                </li>
+                <li>
+                    <Link href='/news/something-else'>Somthing else</Link>
+                </li>
+            </ul>
+        </Fragment>
+    );
 };
 
 export default NewsPage;
